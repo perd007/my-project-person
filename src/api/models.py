7d897070e4966a20b3056a1dc25710d10954be83
jose carlos perdomo herrera <phpperdomo@gmail.com>
2023-12-03 23:34:44 -0400

    creando modelos

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,45 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Patient(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    id_card= db.Column(db.Interger, unique=True, nullable=False)
+    name= db.Column(db.String(120), unique=False, nullable=False)
+    phone= db.Column(db.String(15), unique=False, nullable=False)
+    age= db.Column(db.Interger, unique=False, nullable=False)
+    address=db.Column(db.String(50), unique=False, nullable=False)
+    echo=db.relationship("Echo", backref="patient", lazy=True)
+
+    def __repr__(self):
+        return f'<Patient {self.name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_car": self.id_card,
+            "name": self.name,
+            "phone": self.phone,
+            "age": self.age,
+            "address": self.address,
+        }
+    
+
+    class Echo(db.Model):
+        id= db.Column(db.Interger, primary_key=True)
+        type=db.Column(db.String(50), unique=True, nullable=False)
+        date=db.Column(db.Date, unique=True, nullable=False)
+        pay=db.Column(db.Interger, unique=True, nullable=False)
+        id_patient=db.Column(db.Interger, db.ForeignKey("patient.id"), nullable=False)
+       
+        def __repr__(self):
+            return f'<Type {self.type}>'
+        
+        def serialize(self):
+            return{
+                "id": self.id,
+                "type": self.type,
+                "date": self.date,
+                "day": self.pay,
+            }
